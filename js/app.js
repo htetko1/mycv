e4cac826807456fb293120073df7c5dbd497aba8
Htet Ko <93483021+htetko1@users.noreply.github.com>
2022-07-08 13:59:41 +0630

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let value = 0;
 // trail index number
 let trailValue = 0;
 // interval (Duration)
-let interval = 110000;
+let interval = 5000;
 
 // Function to slide forward
 const slide = (condition) => {
